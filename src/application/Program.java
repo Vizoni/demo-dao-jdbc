d8fb7c02752be1c5ce877c49db09496a6ae1bcd7
Raphael Vizoni do Prado <ravizoni@gmail.com>
2026-09-01 22:16:55 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,12 +4,11 @@ import model.entities.Department;
 import model.entities.Seller;
 
 void main() {
-    Department dpt = new Department(1, "Books");
-
-    Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, dpt);
 
     SellerDao sellerDao = DaoFactory.createSellerDao();
 
-    System.out.println(dpt);
+    Seller seller = sellerDao.findById(3);
+
     System.out.println(seller);
+
 }
