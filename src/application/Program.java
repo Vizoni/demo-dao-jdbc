3132a91723bd991806c8b6cc76316deedec44b41
Raphael Vizoni do Prado <ravizoni@gmail.com>
2026-09-01 21:41:13 -0300

    seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,11 @@
 import model.entities.Department;
+import model.entities.Seller;
 
 void main() {
     Department dpt = new Department(1, "Books");
+
+    Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, dpt);
+
     System.out.println(dpt);
+    System.out.println(seller);
 }
