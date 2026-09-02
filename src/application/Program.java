bb6ae6e645bc85c0ac91a999d2397142ce7b560c
Raphael Vizoni do Prado <ravizoni@gmail.com>
2026-09-01 21:50:15 -0300

    sellerdaojdb and daofactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,3 +1,5 @@
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -5,6 +7,8 @@ void main() {
     Department dpt = new Department(1, "Books");
 
     Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, dpt);
+
+    SellerDao sellerDao = DaoFactory.createSellerDao();
 
     System.out.println(dpt);
     System.out.println(seller);
