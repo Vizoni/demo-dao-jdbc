1301a4c31de203009a175d2fab0266aca327c327
Raphael Vizoni do Prado <ravizoni@gmail.com>
2026-09-03 20:41:21 -0300

    reuse instantiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,6 +7,7 @@ void main() {
 
     SellerDao sellerDao = DaoFactory.createSellerDao();
 
+    System.out.println("---- TEST 1: seller by ID ------");
     Seller seller = sellerDao.findById(3);
 
     System.out.println(seller);
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -50,18 +50,8 @@ public class SellerDaoJDBC implements SellerDao {
             rs = st.executeQuery();
             // rs.next se for true é que tem algum resultado no banco
             if (rs.next()) {
-                Department dep = new Department();
-                dep.setId(rs.getInt("DepartmentId"));
-                dep.setName(rs.getString("DepName"));
-
-                Seller sel = new Seller();
-                sel.setId(rs.getInt("Id"));
-                sel.setName(rs.getString("Name"));
-                sel.setEmail(rs.getString("Email"));
-                sel.setBaseSalary(rs.getDouble("BaseSalary"));
-                sel.setBirthDate(rs.getDate("BirthDate"));
-                sel.setDepartment(dep);
-
+                Department dep = instantiateDepartment(rs);
+                Seller sel = instantiateSeller(rs, dep);
                 return sel;
             }
             return null;
@@ -72,6 +62,26 @@ public class SellerDaoJDBC implements SellerDao {
             DB.closeResultSet(rs);
         }
 
+    }
+
+    private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {
+        Seller sel = new Seller();
+        sel.setId(rs.getInt("Id"));
+        sel.setName(rs.getString("Name"));
+        sel.setEmail(rs.getString("Email"));
+        sel.setBaseSalary(rs.getDouble("BaseSalary"));
+        sel.setBirthDate(rs.getDate("BirthDate"));
+        sel.setDepartment(dep);
+        return sel;
+    }
+
+    // o throws sql exception vai fazer o erro daqui propagar
+    // para o local que está chamando essa função, já q lá tem o try catch
+    private Department instantiateDepartment(ResultSet rs) throws SQLException {
+        Department dep = new Department();
+        dep.setId(rs.getInt("DepartmentId"));
+        dep.setName(rs.getString("DepName"));
+        return dep;
     }
 
     @Override
